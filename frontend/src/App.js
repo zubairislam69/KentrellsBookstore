e9144bc029507d9c65c68b6f163163e4207bbcbe
SabehKhalid1 <sabeh.khalid13@gmail.com>
2022-11-15 15:39:37 -0500

    change-to-Title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Welcome To Kentrells Bookstore
+          WElcome To Kentrells Bookstore
         </p>
       </header>
     </div>
