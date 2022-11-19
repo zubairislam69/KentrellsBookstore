d967c2d36c6fd423767189cf38d3b24ad3430acb
SabehKhalid1 <sabeh.khalid13@gmail.com>
2022-11-19 16:53:23 -0500

    nothing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          WElcome To Kentrells Bookstore
+          Welcome To KeNtrells Bookstore
         </p>
       </header>
     </div>
